2fdf0f5e745ae5a0df27b2de7f0edd4731621cda
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-10-16 13:36:40 -0300

    Camada repository e primeira requisição HTTP post

--- src/main/java/br/com/vinixavier/todolist/user/UserController.java
+++ src/main/java/br/com/vinixavier/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.vinixavier.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository repository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel user) {
-        System.out.println(user.getUsername());
+    public UserModel create(@RequestBody UserModel user) {
+        var userCreated = repository.save(user);
+        return userCreated;
     }
 }
