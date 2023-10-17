2fa3ca99f0114fa6ed1985c085581ad581f658af
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-10-17 00:10:54 -0300

    Criação da model de tasks e tabela no banco de dados

--- src/main/java/br/com/vinixavier/todolist/task/TaskModel.java
+++ src/main/java/br/com/vinixavier/todolist/task/TaskModel.java
@@ -1,10 +1,24 @@
 package br.com.vinixavier.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "tb_tasks")
+@Data
 public class TaskModel {
 
     /*O QUE UMA TAREFA PODE TER ?
        ID
-     * USUARIO
+     * USUARIO(ID_usuario)
      * DESCRIÇÃO
      * TÍTULO
      * DATA DE INICIO
@@ -12,5 +26,25 @@ public class TaskModel {
      * PRIORIDADE 
      * 
     */
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private String priority;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+
+    
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    
+
     
 }
